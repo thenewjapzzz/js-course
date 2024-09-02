8f31eb38ae65c60badc2dbfa3d8b91aca1ac2e75
Guilherme Sato <142543292+thenewjapzzz@users.noreply.github.com>
2024-09-02 18:05:51 -0300

    exercício de função

---
+++ module1/ex/ex5.js
@@ -0,0 +1,9 @@
+let celcius = 35;
+let farenheit;
+function celciusToFahrenheit(celcuis){
+    farenheit = ((celcius * 9) / 5) + 32;
+    return farenheit;
+}
+
+farenheit = celciusToFahrenheit(farenheit);
+console.log(farenheit);
