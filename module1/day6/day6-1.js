be4b1e064171797cb63c3fcc8b4fb4533fc70b36
Guilherme Sato <142543292+thenewjapzzz@users.noreply.github.com>
2024-09-03 23:39:53 -0300

    dia 6 do curso de JS - outros tipos de função

---
+++ module1/day6/day6-1.js
@@ -0,0 +1,14 @@
+// Function Expression
+// Arrow function
+
+const darUmAlo = function(){
+    console.log("E aê, rapaziada");
+}
+
+darUmAlo();
+
+const darUmTchau = () => {
+    console.log("Até mais, rapaziada");
+}
+
+darUmTchau();
