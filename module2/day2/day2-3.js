aa8be2c5e4848b3b5a1db702e99c7f3d3dd74957
thenewjapzzz <guilherme.yokota20@gmail.com>
2024-09-12 21:29:45 -0300

    Exemplo de exercício

--- module2/day2/day2-3.js
+++ module2/day2/day2-3.js
@@ -8,3 +8,22 @@ frutas.unshift("Banana") // Retorna o elemento no primeiro índice
 console.log(frutas);
 frutas.pop();
 console.log(frutas) // Remove o último elemento do array
+
+
+// Exemplo:
+
+const lista1 = ['morango', 'uva', 'maçã'];
+const lista2 = ['amora', 'pêra', 'laranja'];
+
+const frutasss = lista1.concat(lista2);
+console.log(frutasss);
+
+frutasss.splice(1,3);
+console.log(frutasss) // morango, pêra e laranja
+
+frutasss.splice(2, 0, 'melancia', 'acerola');
+console.log(frutasss); // morango, pêra, melancia. acerola. laranja
+
+const novasFrutas = frutasss.slice(2)
+
+console.log(novasFrutas);
